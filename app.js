927b7663c93188ee014697e4f16e5c8c1395cf12
SergejXInfo <157300206+SergejXInfo@users.noreply.github.com>
2024-01-23 20:36:37 +0100

    yeeesssss

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('#mobile-menu')
-const menulinks = document.querySelector('-navbar__menu')
+const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
-    menulinks.classList.toggle('active');
+    menuLinks.classList.toggle('active');
 });
